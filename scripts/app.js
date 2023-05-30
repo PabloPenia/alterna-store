8a7e5430371c34aa0356e85fb44166813b3d09a4
Pablo <pablopenia.dev@gmail.com>
2023-05-30 00:28:24 -0300

    added events

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,15 @@
 function notImplemented() {
 	return alert('Function not implemented.')
 }
+
+const cartBtn = document.getElementById('cartBtn')
+const userBtn = document.getElementById('userBtn')
+
+cartBtn.addEventListener('click', () => showUi(cartBtn))
+userBtn.addEventListener('click', () => showUi(userBtn))
+
+function showUi(btn) {
+	btn.nextElementSibling.style.display === 'block'
+		? (btn.nextElementSibling.style.display = 'none')
+		: (btn.nextElementSibling.style.display = 'block')
+}
