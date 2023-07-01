98aa5d817cc70704b8a6cc60b68bcc53ddc9b7b5
Pablo Peña <pablopenia.dev@gmail.com>
2023-07-01 14:01:34 -0300

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+import data from '../data/productos.json'
+
 function notImplemented() {
 	return alert('Function not implemented.')
 }
@@ -12,4 +14,27 @@ function showUi(btn) {
 	btn.nextElementSibling.style.display === 'block'
 		? (btn.nextElementSibling.style.display = 'none')
 		: (btn.nextElementSibling.style.display = 'block')
+}
+
+useDisplayProducts('novedades', 'image')
+
+function useDisplayProducts(el, output) {
+	const container = document.getElementById(el)
+	if (!container) {
+		throw new Error('El elemento especificado no existe.')
+	}
+
+	let html = ``
+
+	data.forEach((item) => {
+		html += `<article>`
+		switch (output) {
+			case 'image':
+				html += `<img src="assets/images/${item.image}" alt=${item.name}>`
+			default:
+				break
+		}
+		html += `</article>`
+	})
+	container.innerHTML = html
 }
